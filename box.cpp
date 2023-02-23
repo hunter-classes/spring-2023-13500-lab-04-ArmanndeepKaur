a8902f3bc15ae200d49aa49f3ebd004e55b91682
ArmanndeepKaur <armanndeep.22@gmail.com>
2023-02-22 22:13:24 -0500

    added box.cpp

---
+++ box.cpp
@@ -0,0 +1,19 @@
+//Task A
+
+#include <iostream>
+
+
+std::string box(int width, int height)
+{
+
+}
+
+int w, h; 
+
+int main()
+{
+    w = 7;
+    h = 4;
+    std::cout<<box(w,h)
+    return 0;
+}
