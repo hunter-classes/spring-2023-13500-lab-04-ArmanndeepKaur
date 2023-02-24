9a3268e465189e90de768705b15ce0db93caab37
Armanndeep Kaur <104866931+ArmanndeepKaur@users.noreply.github.com>
2023-02-24 04:30:28 +0000

    added files

---
+++ funcs.h
@@ -0,0 +1,10 @@
+#pragma once 
+#include <string>
+
+std::string box(int width, int height); 
+std::string checkerBoard(int width, int height); 
+std::string cross(int size); 
+std::string lower(int sideLength);
+std::string upper(int sideLength); 
+std::string trapezoid(int width, int height);
+std::string checkerBoard3x3(int width, int height); 
